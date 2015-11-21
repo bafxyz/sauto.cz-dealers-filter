609bd1457f9347932e49771dfc953fa5d1f76b4f
Andrei Bunulu <bunulu@gmail.com>
2015-11-22 00:10:08 +0200

    Updated background.js

--- background.js
+++ background.js
@@ -4,7 +4,5 @@
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript(null, {file : 'script.js'});
 });
